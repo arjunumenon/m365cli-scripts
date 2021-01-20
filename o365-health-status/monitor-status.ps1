026fc6ba68aaace164bc00a3c9f0a3bea8b5cc0a
Arjun U Menon <arjun.umenon@gmail.com>
2021-01-20 22:29:46 +0530

    Completed List item addition

--- o365-health-status/monitor-status.ps1
+++ o365-health-status/monitor-status.ps1
@@ -12,8 +12,7 @@ Function getStatus(){
 
 Function getStatusJMES(){
     $workLoads = m365 tenant status list --query "value[?Status != 'ServiceOperational']"  --output json  | ConvertFrom-Json
-    $workLoads | ForEach-Object { Write-Output "WorkLoad: $($_.WorkloadDisplayName )"}
-
+    $workLoads | ForEach-Object { $AddedItem = m365 spo listitem add --contentType Item --listTitle "O365 Health Status" --webUrl https://aum365.sharepoint.com/sites/M365CLI --Title $_.WorkloadDisplayName}
 }
 
 #Check the Login Status
