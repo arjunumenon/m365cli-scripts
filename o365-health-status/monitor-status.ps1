770399939c68223e7b40c2dc7389dd7106f6d982
Arjun U Menon <arjun.umenon@gmail.com>
2021-01-21 15:27:47 +0530

    Added the code for deletion as well

--- o365-health-status/monitor-status.ps1
+++ o365-health-status/monitor-status.ps1
@@ -13,11 +13,14 @@ Function getStatus(){
 Function getStatusJMES(){
     $webURL = "https://aum365.sharepoint.com/sites/M365CLI"
     $listName = "O365 Health Status"
-    $workLoads = m365 tenant status list --query "value[?Status != 'ServiceOperational']"  --output json  | ConvertFrom-Json
+    $workLoads = m365 tenant status list --query "value[]"  --output json  | ConvertFrom-Json
     $currentOutageServices = (m365 spo listitem list --webUrl $webURL --title $listName --fields "Title, Workload, Id"  --output json).Replace("ID", "_ID") | ConvertFrom-Json
 
-    #Deleting the list item if the service is back to normal
+    #Updating the status to IsInOutage to NO the list item if the service is back to normal
     $currentOutageServices | ?{$_.Workload -notin $workLoads.Workload} | %{ $UpdatedWorkflod = m365 spo listitem set --webUrl $webURL --listTitle $listName --contentType Item --id  $_.Id --StillinOutage "false" }
+
+    #Deleting the record if not needed
+    #$currentOutageServices | ?{$_.Workload -notin $workLoads.Workload} | %{ $UpdatedWorkflod = m365 spo listitem remove --webUrl $webURL --listTitle $listName --id  $_.Id }
 
     #Adding items if any of the workload is not availble
     $workLoads | ?{$_.Workload -notin $currentOutageServices.Workload} | %{ $AddedItem = m365 spo listitem add --webUrl $webURL --listTitle $listName --contentType Item --Title $_.WorkloadDisplayName --Workload $_.Workload --FirstIdentifiedDate (Get-Date -Date $_.StatusTime -Format "MM/dd/yyyy HH:mm") --WorkflowJSONData (Out-String -InputObject $_ -Width 100) }
