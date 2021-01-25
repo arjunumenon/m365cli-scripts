70b8da3f833ab82120a352f72a7f540457125909
Arjun U Menon <arjun.umenon@gmail.com>
2021-01-25 14:50:09 +0530

    Completed Solution

--- o365-health-status/monitor-status-CliNotify.ps1
+++ o365-health-status/monitor-status-CliNotify.ps1
@@ -14,14 +14,14 @@ $currentOutageServices = (m365 spo listitem list --webUrl $webURL --title $listN
 
 Foreach ($workload in $workLoads){
     if($workload.Workload -notin $currentOutageServices.Workload){
-        $AddedItem = m365 spo listitem add --webUrl $webURL --listTitle $listName --contentType Item --Title $workload.WorkloadDisplayName --Workload $workload.Workload --FirstIdentifiedDate (Get-Date -Date $workload.StatusTime -Format "MM/dd/yyyy HH:mm") --WorkflowJSONData (Out-String -InputObject $workload -Width 100)
+        $addedWorkLoad = m365 spo listitem add --webUrl $webURL --listTitle $listName --contentType Item --Title $workload.WorkloadDisplayName --Workload $workload.Workload --FirstIdentifiedDate (Get-Date -Date $workload.StatusTime -Format "MM/dd/yyyy HH:mm") --WorkflowJSONData (Out-String -InputObject $workload -Width 100)
 
         #Send notification using CLI Commands
         m365 outlook mail send --to "arjun@aum365.onmicrosoft.com" --subject "Outage Reported in $($workload.WorkloadDisplayName)" --bodyContents "Any outage has been reported for the Service : $($workload.WorkloadDisplayName) <a href='https://aum365.sharepoint.com/sites/M365CLI/Lists/O365%20Health%20Status'>Access the Health Status List</a>" --bodyContentType HTML
     }
 }
 
- #Updating the status to IsInOutage to NO the list item if the service is back to normal
+#Updating the status to IsInOutage to NO the list item if the service is back to normal
 Foreach ($currentOutageService in $currentOutageServices){
     if($currentOutageService.Workload -notin $workLoads.Workload){
         $RemovedRecord = m365 spo listitem remove --webUrl $webURL --listTitle $listName --id  $currentOutageService.Id
--- o365-health-status/monitor-status.ps1
+++ o365-health-status/monitor-status.ps1
@@ -13,7 +13,7 @@ $workLoads = m365 tenant status list --query "value[?Status != 'ServiceOperation
 $currentOutageServices = (m365 spo listitem list --webUrl $webURL --title $listName --fields "Title, Workload, Id"  --output json).Replace("ID", "_ID") | ConvertFrom-Json
 
 #Adding items if any of the workload is not availble
-$workLoads | ?{$_.Workload -notin $currentOutageServices.Workload} | %{ $AddedItem = m365 spo listitem add --webUrl $webURL --listTitle $listName --contentType Item --Title $_.WorkloadDisplayName --Workload $_.Workload --FirstIdentifiedDate (Get-Date -Date $_.StatusTime -Format "MM/dd/yyyy HH:mm") --WorkflowJSONData (Out-String -InputObject $_ -Width 100) }
+$workLoads | ?{$_.Workload -notin $currentOutageServices.Workload} | %{ $addedWorkLoad = m365 spo listitem add --webUrl $webURL --listTitle $listName --contentType Item --Title $_.WorkloadDisplayName --Workload $_.Workload --FirstIdentifiedDate (Get-Date -Date $_.StatusTime -Format "MM/dd/yyyy HH:mm") --WorkflowJSONData (Out-String -InputObject $_ -Width 100)}
 
  #Updating the status to IsInOutage to NO the list item if the service is back to normal
 $currentOutageServices | ?{$_.Workload -notin $workLoads.Workload} | %{ $UpdatedWorkflod = m365 spo listitem set --webUrl $webURL --listTitle $listName --contentType Item --id  $_.Id --StillinOutage "false" }
